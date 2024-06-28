97a16efff5fce53a770f58a90fcbb8297a0fdab3
ThaminduNethmina <himanshavithanage@gmail.com>
2024-06-28 19:06:39 +0530

    feat: Added tooltips to sidebar navigations using Flowbite

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from 'react-router-dom'
 import navigations from '../../lib/consts/navigations'
 import { HiOutlineArrowLeftStartOnRectangle } from "react-icons/hi2";
+import { Tooltip } from 'flowbite-react';
 
 function Sidebar() {
     const { pathname } = useLocation()
@@ -12,16 +13,20 @@ function Sidebar() {
             <div className="flex-1 py-5">
                 {
                     navigations.map(nav => (
-                        <Link to={nav.to} key={nav.key} title={nav.name} className={`flex items-center justify-center ${pathname === nav.to ? "text-white bg-slate-700" : "text-neutral-400" }  hover:bg-slate-700 text-2xl p-3 transition ease-out duration-500 hover:translate-x-3`}>
-                            {nav.icon}
+                        <Link to={nav.to} key={nav.key} className={`flex items-center justify-center ${pathname === nav.to ? "text-white bg-slate-700" : "text-neutral-400"}  hover:bg-slate-700 text-2xl p-3 transition ease-out duration-500`}>
+                            <Tooltip content={nav.name} placement="right" className="ms-5">
+                                {nav.icon}
+                            </Tooltip>
                         </Link>
-                    )) 
+                    ))
                 }
             </div>
             <div className="flex justify-center border-t-2 border-t-gray-400">
-                <button title="Logout" className="text-center text-red-300 hover:bg-slate-700 text-2xl w-full p-3 transition ease-out duration-500">
-                    <HiOutlineArrowLeftStartOnRectangle className="mx-auto" />
-                </button>
+                <Tooltip content="Logout" placement="right">
+                    <button title="Logout" className="text-center px-5 text-red-300 hover:bg-slate-700 text-2xl p-3 transition ease-out duration-500">
+                        <HiOutlineArrowLeftStartOnRectangle className="mx-auto" />
+                    </button>
+                </Tooltip>
             </div>
         </div>
     )
